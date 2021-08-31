2e86aa322c96af16564f6674cdb10de8d8a225b0
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-08-30 21:22:26 -0700

    Start-Button functions/rough draft

--- js/app.js
+++ js/app.js
@@ -1,67 +1,7 @@
 'use strict'
 
-console.log('Do you remember?');
 
-//    global variables
-let introScreen = document.getElementById('homeScreen');
-let newGameButtonElem  = document.getElementById('newGameButton');
-let submitAnswerButtonElem = document.getElementById('submitAnswerButton');
-let loadGameButtonElem
-
-let pElem = document.getElementById('storyP')
-let formSectionElem = document.getElementById('formSection')
-
-let currentQuestion = 0;
-//need here or inside Question constructor?
-// let questionsArray = [];
-
-
-//function to create a user
-function User(name, role) {
- this.name = name;
- this.role = role;
- this.lives = 3;
- this.curentQuestion = [];
-}
-
-let currentUser = new User(name, role)
-
-
-//Story/Lore constructor function
-function Story(backgroundImage) {
- this.backgroundImage = backgroundImage;
-}
-
-//Question constructor function
-function Question(questionNumber) {
- this.questionNumber = questionNumber
- this.questionIndex = 0;
- this.questionsArray = [];
-}
-
-Question.prototype.addQuestion = function(text) {
- this.questionsArray.push(text)
-}
-
-function addQuestion() {
-
-}
-
-Question.prototype.getNextQuestion = function() {
- 
-}
-
-function renderQuestion() {
- let firstQuestion = []
- let secondQuestion = []
- let thirdQuestion = []
- let fourthQuestion = []
- let fifthQuestion = []
-}
-
-//render question to the page?
-Question.prototype.renderQuestion() {
-}
+// Everything in here as of 8.30.21 is not being utilized in any files. As of right now this is a reference page for possible functions down the road. 
 
 //creating button to start the game
 // function startGameButton() {
@@ -78,80 +18,149 @@ Question.prototype.renderQuestion() {
 //     let startTheGameButton = document.createElement('button');
 // }
 
-function startGameButton() {
-
-}
-
-//starting the game
-function startGame() {
-
-}
-
-//function to load game
-function loadGame(event) {
-
-}
-
-//function reset the game
-function resetGame() {
-
-}
-
-//function for saving game
-function saveGame() {
-
-}
-
-// local storage to save game and user info
-function storeUserData() {
- const stringifiedUser = JSON.stringify(User.allusers);
- localStorage.setItem('usersInStorage', stringifiedUser)
-}
-
-function getUserFromStorage() {
- const stringifiedUser = localStorage.getItem('usersInStorage')
- if(usersInStorage) {
-     const parsedUsers = JSON.parse(stringifiedUsers)
-     for(let user of parsedUsers) {
-         const currentUserName = user.name
-         for(let originalUser of User.allUsers) {
-             const originalUser = originalUser.name
-             if(currentUserName === originalUser) 
-             originalUser.name = user.name;
-             originalUser.role = user.role;
-             originalUser.lives = user.lives;
-         }
-     }
- }
-}
 
 
-//function for creating next question/npc and background image
-function nextQuestion(npcName, npcBackgroundImage) {
+// console.log('Do you remember?');
 
- this.characterName = npcName;
- this.npcBackgroundImage = npcBackgroundImage
-}
+// // ----------------------------------------------------------global variables ---------------------------------------------------------------
+// let introScreen = document.getElementById('homeScreen');
+// let newGameButtonElem  = document.getElementById('newGameButton');
+// let submitAnswerButtonElem = document.getElementById('submitAnswerButton');
+// let loadGameButtonElem
 
-// function for adding next question to page
-function renderQuestions() {
+// let pElem = document.getElementById('storyP')
+// let formSectionElem = document.getElementById('formSection')
 
-let questionOne = '';
+// let currentQuestion = 0;
 
-let questionTwo = '';
+// // ----------------------------------------------------------Constructors ---------------------------------------------------------------
+// function User(name, role) {
+//     this.name = name;
+//     this.role = role;
+//     this.lives = 3;
+//     this.curentQuestion = [];
+//    }
 
-let questionThree = '';
+// let currentUser = new User(name, role)
 
-let questionFour = '';
+// // function for adding next question to page
+// function renderQuestions() {
 
-let questionFive = '';
+//     let questionOne = '';
+    
+//     let questionTwo = '';
+    
+//     let questionThree = '';
+    
+//     let questionFour = '';
+    
+//     let questionFive = '';
+    
+//     }
 
-}
+
+// // ----------------------------------------------------------Functions ---------------------------------------------------------------
+// //function to create a user
+
+// //function for creating next question/npc and background image
+// function nextQuestion(npcName, npcBackgroundImage) {
+
+//     this.characterName = npcName;
+//     this.npcBackgroundImage = npcBackgroundImage
+//    }
+   
+// function User(name, role) {
+//     this.name = name;
+//     this.role = role;
+//     this.lives = 3;
+//     this.curentQuestion = [];
+//    }
+   
+   
+//    //Story/Lore constructor function
+//    function Story(backgroundImage) {
+//     this.backgroundImage = backgroundImage;
+//    }
+   
+//    //Question constructor function
+//    function Question(questionNumber) {
+//     this.questionNumber = questionNumber
+//     this.questionIndex = 0;
+//     this.questionsArray = [];
+//    }
+   
+//    Question.prototype.addQuestion = function(text) {
+//     this.questionsArray.push(text)
+//    }
+   
+//    function addQuestion() {
+   
+//    }
+   
+//    function renderQuestion() {
+//     let firstQuestion = []
+//     let secondQuestion = []
+//     let thirdQuestion = []
+//     let fourthQuestion = []
+//     let fifthQuestion = []
+//    }
+   
+//    function startGameButton() {
+
+// }
+
+// //starting the game
+// function startGame() {
+
+// }
+
+// //function to load game
+// function loadGame(event) {
+
+// }
+
+// //function reset the game
+// function resetGame() {
+
+// }
+
+// //function for saving game
+// function saveGame() {
+
+// }
+
+// // local storage to save game and user info
+// function storeUserData() {
+//     const stringifiedUser = JSON.stringify(User.allusers);
+//     localStorage.setItem('usersInStorage', stringifiedUser)
+//    }
+   
+//    function getUserFromStorage() {
+//     const stringifiedUser = localStorage.getItem('usersInStorage')
+//     if(usersInStorage) {
+//         const parsedUsers = JSON.parse(stringifiedUsers)
+//         for(let user of parsedUsers) {
+//             const currentUserName = user.name
+//             for(let originalUser of User.allUsers) {
+//                 const originalUser = originalUser.name
+//                 if(currentUserName === originalUser) 
+//                 originalUser.name = user.name;
+//                 originalUser.role = user.role;
+//                 originalUser.lives = user.lives;
+//             }
+//         }
+//     }
+//    }
 
 
-newGameButtonElem.addEventListener('click', //whatever our handleClick func is)
-loadGameButtonElem.addEventListener('click', //same)
+// // ----------------------------------------------------------Prototypes---------------------------------------------------------------
+// Question.prototype.getNextQuestion = function() {  
+// }
 
-renderQuestion() {
 
-}
+// ----------------------------------------------------------Listeners ---------------------------------------------------------------
+
+
+
+
+
--- js/questions.js
+++ js/questions.js
@@ -1,122 +1,199 @@
-let story = {
-    currentQuestion: 'Question One',
-    question: {
-        currentQuestion: 'The Mysterious Figure',
-        story: 'this is question 1',
-        choices: [
-            {
-                choice: 'a',
-                choice: 'b',
-                choice: 'c',
-                choice: 'd',
-                destination: 'questionTwo',
-            },
-        ]
-    }, 
-    questionTwo: {
-        currentQuestion: 'The Bartender',
-        story: 'this is question 2',
-        choices: [
-            {
-                choice: 'a',
-                choice: 'b',
-                choice: 'c',
-                choice: 'd',
-                destination: 'questionThree',
-            },
-        ]
-    },
-    questionThree: {
-        currentQuestion: 'The Criminal',
-        story: 'this is question 3',
-        choices: [
-            {
-                choice: 'a',
-                choice: 'b',
-                choice: 'c',
-                choice: 'd',
-                destination: 'questionFour',
-            },
-        ]
-    },
-    questionFour: {
-        currentQuestion: 'The Aquaintance',
-        story: 'this is question 4',
-        choices: [
-            {
-                choice: 'a',
-                choice: 'b',
-                choice: 'c',
-                choice: 'd',
-                destination: 'questionFive',
-            },
-        ]
-    },
-    questionFive: {
-        currentQuestion: 'The Mysterious Figure Again',
-        story: 'this is question 5',
-        choices: [
-            {
-                choice: 'a',
-                choice: 'b',
-                choice: 'c',
-                choice: 'd',
-                destination: 'endGame',
-            },
-        ]
-    },
-    endGame: {
-        currentQuestion: 'The Final Chapter',
-        story: 'this is the game end story',
-        // choices: [
-        //     {
-        //         choice: 'a',
-        //         choice: 'b',
-        //         choice: 'c',
-        //         choice: 'd',
-        //         destination: 'questionFour',
-    //         },
-    //     ]
-    // },
+// --------------------------------------------------------------------- On Page-Load ----------------------------------------------------------------------------
+
+// On Page Load ('DOMContentLoaded') will create our 3 variables bellow and then load an eventlistener that awaits a 'click' on 'start game'. 
+document.addEventListener('DOMContentLoaded', function() {
+  let startButton = document.getElementById('startButton');
+  let loadGame = document.getElementById('loadButton');
+  let gameContainer = document.getElementById('question-container');
+  let imageContainer = document.getElementById('npc-image');
+  let adventureButton = document.getElementById('character-submit');
+
+  startButton.addEventListener('click', function(){
+    gameContainer.innerHTML = `
+      <h1>Hello New Player, What do we call you?<h1>
+      
+      <form> 
+      <label>'Sign Here'</label>
+      <input type="text" id="userName" placeholder="sign on the dotted line">
+      
+      <label for='classSelection'>Select Your Class </label>
+      <select id="classSelection">
+      <option value="artist">Artist</option>
+      <option value="historian">Historian</option>
+      <option value="philosopher">Philosopher</option>
+      <select>
+      <input id="character-submit" type="submit" value="Start your Adventure">
+      <form>`;
+    imageContainer.innerHTML = '<img src=img/traveler.jpeg>';
+  });
+
+});
+//   loadGame.addEventListener('click' function() {})
+
+
+// --------------------------------------------------------------------- Variables --------------------------------------------------------------------------------
+User.allUsers = [];
+
+
+
+// --------------------------------------------------------------------- Objects ----------------------------------------------------------------------------------
+
+/// Questions POJO (Plain-Old-Javascript-Object)
+let question = {
+
+}
+
+/// User Object
+function User(name, role) {
+  this.name = name;
+  this.role = role;
+  this.lives = 3;
+  this.curentQuestion = '';
+}
+
+// --------------------------------------------------------------------- Functions --------------------------------------------------------------------------------
+
+
+
+//// Trying to create a function to take unput from character submit button and create a User and push to User.allUser[];
+function newCharacter(event) {
+  console.log(event.target);
+//   let name = event.target;
+//   let role = event.target;
+//   const newPlayer = new User(name,role);
+//   User.allUsers.push(newPlayer);
+}
+
+
+
+
+
+// --------------------------------------------------------------------- Listeners --------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------- Function Calls ----------------------------------------------------------------------------
+
+
+
+// let story = {
+//     currentQuestion: 'Question One',
+//     question: {
+//         currentQuestion: 'The Mysterious Figure',
+//         story: 'this is question 1',
+//         choices: [
+//             {
+//                 choice: 'a',
+//                 choice: 'b',
+//                 choice: 'c',
+//                 choice: 'd',
+//                 destination: 'questionTwo',
+//             },
+//         ]
+//     }, 
+//     questionTwo: {
+//         currentQuestion: 'The Bartender',
+//         story: 'this is question 2',
+//         choices: [
+//             {
+//                 choice: 'a',
+//                 choice: 'b',
+//                 choice: 'c',
+//                 choice: 'd',
+//                 destination: 'questionThree',
+//             },
+//         ]
+//     },
+//     questionThree: {
+//         currentQuestion: 'The Criminal',
+//         story: 'this is question 3',
+//         choices: [
+//             {
+//                 choice: 'a',
+//                 choice: 'b',
+//                 choice: 'c',
+//                 choice: 'd',
+//                 destination: 'questionFour',
+//             },
+//         ]
+//     },
+//     questionFour: {
+//         currentQuestion: 'The Aquaintance',
+//         story: 'this is question 4',
+//         choices: [
+//             {
+//                 choice: 'a',
+//                 choice: 'b',
+//                 choice: 'c',
+//                 choice: 'd',
+//                 destination: 'questionFive',
+//             },
+//         ]
+//     },
+//     questionFive: {
+//         currentQuestion: 'The Mysterious Figure Again',
+//         story: 'this is question 5',
+//         choices: [
+//             {
+//                 choice: 'a',
+//                 choice: 'b',
+//                 choice: 'c',
+//                 choice: 'd',
+//                 destination: 'endGame',
+//             },
+//         ]
+//     },
+//     endGame: {
+//         currentQuestion: 'The Final Chapter',
+//         story: 'this is the game end story',
+//         // choices: [
+//         //     {
+//         //         choice: 'a',
+//         //         choice: 'b',
+//         //         choice: 'c',
+//         //         choice: 'd',
+//         //         destination: 'questionFour',
+//     //         },
+//     //     ]
+//     // },
     
-}
+// }
 
-//DOMContentLoaded fires when the initial HTML doc has been completely loaded and parsed
-document.addEventListener('DOMContentLoaded', function () {
-    let button = document.getElementById('startButton')
-    let input = document.getElementById('nameInput')
-    let content = Document.getElementById('gameContainer')
-    button.addEventListener('click', renderQuestion)
+// //DOMContentLoaded fires when the initial HTML doc has been completely loaded and parsed
+// document.addEventListener('DOMContentLoaded', function () {
+//     let button = document.getElementById('startButton')
+//     let input = document.getElementById('nameInput')
+//     let content = Document.getElementById('gameContainer')
+//     button.addEventListener('click', renderQuestion)
     
-    function renderQuestion() {
-        content.innerHTML = `
-        <h1>${story[currentQuestion].title}</h1>
-        <p>${story[currentQuestion].story}</p>
-        ${getinputs()}
-        <button id = "submitButton">Submit</button
-        `
-        let buttonElem = document.getElementById('submitButton')
-        buttonElem.addEventListener('click', function() {
+//     function renderQuestion() {
+//         content.innerHTML = `
+//         <h1>${story[currentQuestion].title}</h1>
+//         <p>${story[currentQuestion].story}</p>
+//         ${getinputs()}
+//         <button id = "submitButton">Submit</button
+//         `
+//         let buttonElem = document.getElementById('submitButton')
+//         buttonElem.addEventListener('click', function() {
 
-        })
-    }
+//         })
+//     }
 
-function getInputValue() {
-    let inputs = document.getElementById('input[type = "radio"]')
-    for(let i = 0; i < inputs.length; i++ ) {
-        if(inputs[i].checked)
-    }
+// function getInputValue() {
+//     let inputs = document.getElementById('input[type = "radio"]')
+//     for(let i = 0; i < inputs.length; i++ ) {
+//         if(inputs[i].checked) {
 
-}
+//         };
 
-function getInputs() {
-    let input = "";
-    for(let i = 0; i < story[currentQuestion].choices.length; i++) {
-        choices += `
-        <div>
-        <input id = "radio${i}" type = "radio" name = "choices"/>
-        <label for "radio${i}">${story[story.currentQuestion].choices[i].choice}</label>
-        </div>`
-     }
-     return input;
-}
+// };
+
+// function getInputs() {
+//     let input = "";
+//     for(let i = 0; i < story[currentQuestion].choices.length; i++) {
+//         choices += `
+//         <div>
+//         <input id = "radio${i}" type = "radio" name = "choices"/>
+//         <label for "radio${i}">${story[story.currentQuestion].choices[i].choice}</label>
+//         </div>`
+//      }
+//      return input;
+// }

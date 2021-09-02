21d29e3eb0825198ad4f9ab27844a09f089e5b46
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-09-02 10:34:29 -0700

    start and load game function properly

--- js/questions.js
+++ js/questions.js
@@ -2,7 +2,7 @@
 
 // --------------------------------------------------------------------- On Page-Load-------------------------------------------------------
 
-function initialLoad() {
+function newInitialLoad() {
   let startButton = document.getElementById('startButton');
   let questionContainer = document.getElementById('question-container');
   let imageContainer = document.getElementById('npc-image');
@@ -22,9 +22,9 @@ function initialLoad() {
 
       <label id="role">Select Your Class</label>
       <select name="role" id="classSelection">
-      <option value="img/traveler.jpeg">Artist</option>
-      <option value="historian">Historian</option>
-      <option value="philosopher">Philosopher</option>
+      <option value="img/artistsmpl.png">Artist</option>
+      <option value="img/historiansmpl.png">Historian</option>
+      <option value="img/samplephilosopher.png">Philosopher</option>
       </select>
       </label>
 
@@ -66,20 +66,22 @@ function getDataForm(event) {
 // Creates a new User & pushes this user to allUsers array
 function newCharacter(name,role) {
 const newPlayer = new User(name,role);
-newPlayer.renderUser();
+newPlayer.renderUser('displayName','displayImg');
 User.allUsers.push(newPlayer);
 };
 
 // Render userNAme and Role to the GameContainer
-User.prototype.renderUser = function() {
+User.prototype.renderUser = function(nameElementId,roleElementId) {
   // grab reference to username feild in html
-  let userName = document.getElementById('displayName')
+  let userName = document.getElementById(nameElementId)
   // grab reference to image in html
-  let userRole = document.getElementById('displayImg')
+  let userRole = document.getElementById(roleElementId)
   // update the text content of each element in the field. 
   userName.textContent = this.name
   userRole.src = this.image
 };
+
+
 
 // function to render our question, image to the page; update the path() and render the next question
 function renderQuestions() {
@@ -137,35 +139,68 @@ function path() {
   }
 }
 
+// Renders existing user if there is storage data
+User.prototype.renderExistingUser = function(nameElementId,roleElementId) {
+    // grab reference to username feild in html
+    let userName = document.getElementById(nameElementId)
+    // grab reference to image in html
+    let userRole = document.getElementById(roleElementId)
+    // update the text content of each element in the field. 
+    nameElementId.textContent = "Welcome back " + this.name + ", " +  " Its lovely to see you again...." + " would you like to continue?"
+    roleElementId.textContent = "Click New Game to create a new character or Resume to continue your adventure."
+  };
+
+User.prototype.existingInitialLoad = function() {
+    let welcomeBack = document.getElementById('storyh1');
+    let welcomeText = document.getElementById('storyP');
+    // let existingPlayer = User.allUsers[0]
+
+    this.renderExistingUser(welcomeBack,welcomeText);
+
+};
 
 function storage() {
   const storedUsers = JSON.stringify(User.allUsers)
   localStorage.setItem( 'users', storedUsers);
+
 }
 
 function getStorage() {
   const getUsers = localStorage.getItem('users')
-  // parse the data from local storage
+  let loadButton = document.getElementById('loadButton')
+  let startButton = document.getElementById('startButton');
+
+
   if (getUsers !== null) {
-    // grab reference to the quetion container
-    //grab reference to homepage <h1> & <p>
-    //create a new inner.html on the question container. 
-    // edit the information to say 'weclome back'
-    //post a resume button
-    //have the resume button render the first question rather than the 'select your character'
-  }
+    const parsedGetUsers = JSON.parse(getUsers);
+   for(let users of parsedGetUsers) {
+       const retrievedUsers = new User(users.name,users.image);
+       User.allUsers.push(retrievedUsers)
+   }
+   const existingUser = (User.allUsers[User.allUsers.length -1])
+   console.log(existingUser)
+   existingUser.existingInitialLoad()
+   loadButton.addEventListener('click', function(){
+       renderQuestions();})
+    startButton.addEventListener('click', function(){
+           newInitialLoad();}) 
+    } else {
+        startButton.addEventListener('click', function(){
+            newInitialLoad();})
+    };  
 };
 
 // ------------------------------------------------------- Start of Story ------------------------------------------------------------------
-
+console.log(User.allUsers[0])
 let game = {
   // ------------------------------------------------------- Chapter 1- Denial ----------------------------------------------------------------------
     path: 'Q1',
     Q1: {
       npc: '',
       story: 'You wake as though from a dream that feels to have lasted an eternity, yet any effort to recall the details beyond brief flashes of imagery leave your head spinning. Everything aches down to your very bones. A strange feeling, you think as you raise your hands to wipe the sleep from your eyes, for you do not believe yourself old enough to have aching bones.But for a moment your hands appear old and withered before returning to a youthful state. They are covered in blood. “So you lived,” an otherworldly voice speaks to you. It is neither loud nor soft but sounds right next to you despite the distance between you and the stump upon which they sit. They wear a thick cloak, the hood pulled up to conceal their face, meticulously peeling an apple with a dagger. “You almost had me worried.” When they turn to face you, you can nearly see the gleam in their eyes. As the wind changes, the direction of the light shifts, and you see it filter across their face.',
-      question: 'You know this person. Not them as an individual, but you know their face. You know their deeds. You know why they are here. But that is all you know. You can not say how you ended up in this forest, if you were here to begin within or if you were brought. You do not even know your own name or whose blood stains your trembling hands. The figure approaches, extending a pale hand to offer you an apple. “Trust me, userName this is exactly what you deserve. Are you ready?”',
-        choices: [
+      question: `You know this person. Not them as an individual, but you know their face. You know their deeds. You know why they are here. But that is all you know. You can not say how you ended up in this forest, if you were here to begin within or if you were brought. You do not even know your own name or whose blood stains your trembling hands. The figure approaches, extending a pale hand to offer you an apple. “Trust me, 'username' this is exactly what you deserve. Are you ready?”`,
+      image: '',  
+      choices: [
             {
                 response: 'Yes',
                 path: 'Q2A'
@@ -190,6 +225,7 @@ let game = {
         npc: '',
         story: 'The figure seems to frown and takes a step back. “That doesn’t sound like you at all, Username. You must have hit your head harder than I thought during the journey.”',
         question: 'Did you really expect things to be different this time?',
+        image: '',
         choices: [
             {
                 response: 'I don\'t know what you mean...',
@@ -205,6 +241,7 @@ let game = {
         npc: '',
         story: 'The figure seems to smile beneath their hood and extends the apple further, “How predictable. You said that last time.',
         question: 'Are you beginning to remember?',
+        image: '',
         choices: [
             {
                 response: '...Last time?',
@@ -222,6 +259,7 @@ let game = {
         npc: '',
         story: 'You refuse to play this game, but as a result you are stuck in this forest and cannot find your way out...',
         question: 'Try your luck again?',
+        image: '',
         choices: [
             {
                 response: '*Walk deeper into the forest*',
@@ -235,7 +273,8 @@ let game = {
         npc: '',
         story: 'Despite your protests, the hooded figure shoves the apple in your hands. When you look down, it\'s not longer an apple, but a book bearing your name on the cover. You look up and the scenery has changed. The forest is gone and you\'re standing in the middle of a town square. The figure points out a person and you realize it\'s a younger version of you. You watch as they argue with a shopkeeper and flinch knowing that the punch is coming. As you watch the blood pooling under your younger selfs head, the figure asks:',
         question: '"Looking back now, do you think this was deserved?"',
-          choices: [
+        image: '',  
+        choices: [
               {
                   response: 'What is happening??',
                   path: 'Q4A'
@@ -261,6 +300,7 @@ let game = {
         npc: '',
         story: 'The figure pauses and merely shrugs. "If you haven\t figured it out yet, you will by the time we\re done.',
         question: '"Are you alright with that?"',
+        image: '',
         choices: [
             {
                 response: 'I suppose I have to be at this point.',
@@ -276,6 +316,7 @@ let game = {
         npc: '',
         story: 'Looks like you\'ve shown some growth this time around.',
         question: '"Let\'s continue."',
+        image: '',
         choices: [
             {
                 response: 'It doesn\'t look like I seem to have a choice',
@@ -287,6 +328,7 @@ let game = {
         npc: '',
         story: 'The Figure chuckles lightly and replies, "You would say that."',
         question: 'Could there have been a better way?',
+        image: '',
         choices: [
             {
                 response: 'Yes there could\'ve been.',
@@ -310,6 +352,7 @@ let game = {
         npc: '',
         story: 'The Figure merely laughs and strangely your punch doesn\'t even make contact.',
         question: 'Are you done with your temper tantrum?',
+        image: '',
         choices: [
             {
                 response: 'You\'re not gonna win me over with this.',
@@ -325,6 +368,7 @@ let game = {
         npc: '',
         story: 'The figure stands by as you pace, blowing off steam, allowing you to calm down. After a few minutes, the figure continues on.',
         question: '',
+        image: '',
         choices: [
             {
                 response: 'Follow the Figure.',
@@ -337,6 +381,7 @@ let game = {
         npc: '',
         story: 'The figure no longer seems to be listening and walks away. The only way to get answers is to play his game, so you follow after him.',
         question: '',
+        image: '',
         choices: [
             {
                 response: 'Continue on...',
@@ -351,7 +396,8 @@ let game = {
         npc: '',
         story: 'The cloaked figure ushers you into a church. You know this church. You have been here before, and you see that version of yourself sitting in a pew. The memories are beginning to return the longer you are with this figure. You know what is coming. You remember what led you to this place before. You are beginning to understand the purpose behind this figure’s game.',
         question: 'Do you still blame yourself?',
-          choices: [
+        image: '',  
+        choices: [
               {
                   response: 'It was never my fault.',
                   path: 'Q6A'
@@ -378,6 +424,7 @@ let game = {
         npc: '',
         story: 'The Figure remains silent for a moment. He almost seems disappointed.',
         question: 'Is that what you really think?',
+        image: '',
         choices: [
             {
                 response: 'No...',
@@ -393,6 +440,7 @@ let game = {
         npc: '',
         story: 'The Figures eyes seem to shine and you swear you actually see him smile.',
         question: 'Then why didn\t you stop them?',
+        image: '',
         choices: [
             {
                 response: 'I didn\'t know...',
@@ -408,6 +456,7 @@ let game = {
         npc: '',
         story: 'The figure sighs and shakes his head. He says, "The signs were there. If you weren\'t so self involved you would\'ve seen them."',
         question: 'How do you respond?',
+        image: '',
         choices: [
             {
                 response: 'That isn\'t how it happened!',
@@ -423,6 +472,7 @@ let game = {
         npc: '',
         story: 'The figure nods solemnly and you know your former self is thinking the same. In time you\'ll come to peace with what transpired.',
         question: '',
+        image: '',
         choices: [
             {
                 response: 'Continue on.',
@@ -437,6 +487,7 @@ let game = {
         npc: '',
         story: 'The figure appears angered. Raising his voice he says, "You\'ll never learn."',
         question: 'Try again?',
+        image: '',
         choices: [
             {
                 response: 'Continue on...',
@@ -450,7 +501,8 @@ let game = {
         npc: '',
         story: 'You follow the figure out of the church, but the moment you step outside, you\'re in your former home. You see your younger self at the table, but they do not notice you know. They are preoccupied with their drink. This is something you remember, but you cannot say how many drinks you had, or what other substances you consumed. Even without looking in the book, you could easily identify what page this event takes place on.',
         question: 'Did you ever expect it to get to this point?',
-          choices: [
+        image: '',  
+        choices: [
               {
                   response: 'No. Why didn\'t anyone stop me?',
                   path: 'Q8A'
@@ -476,6 +528,7 @@ Q8A: {
     npc: '',
     story: 'The Figure scoffs and shakes his head yet again as if he can\'t believe nothing has changed.He says, "you act as though the universe had some grand plan, but life doesn\'t work that way."',
     question: 'What makes you so important?',
+    image: '',
     choices: [
         {
             response: 'Aren\'t we all important?',
@@ -491,6 +544,7 @@ Q8B: {
     npc: '',
     story: 'The Figure replies, "Anything is with the wrong motivations."',
     question: 'Do you regret what happened up to this point?',
+    image: '',
     choices: [
         {
             response: 'Absolutely.',
@@ -506,6 +560,7 @@ Q8C: {
     npc: '',
     story: '"How pitiful," The Figure replies. "You truly are wasted potential.',
     question: 'Do you have any regrets?',
+    image: '',
     choices: [
         {
             response: 'Yes.',
@@ -523,6 +578,7 @@ GameOver3: {
     npc: '',
     story: 'Clearly we have more work to do. Let\'s try this again.',
     question: '',
+    image: '',
     choices: [
         {
             response: 'Continue?',
@@ -537,7 +593,8 @@ Q9: {
     npc: '',
     story: 'It\'s at this point that your younger self finally notices your presence. More specifically, the cloaked figure beside you. They become distraught as if they know what\'s coming next. You turn away not wanting to see, not wanting to hear, not wanting to play this game anymore. Your heart pounds in your ears and the moment you blink, you are back in the forest. The figure holds out another apple, "I\'m sorry you had to see that."',
     question: 'Would you have done it any differently?',
-      choices: [
+    image: '',  
+    choices: [
           {
               response: 'Yes I would.',
               path: 'Q9A'
@@ -556,7 +613,8 @@ Q9: {
     npc: '',
     story: 'Alas, life doesn\'t work that way. You still have some improvements to make, but you\'ve come this far. I think you\'ve suffered enough.',
     question: 'learn your fate?',
-      choices: [
+    image: '',  
+    choices: [
           {
               response: 'Yes.',
               path: 'Q10A'
@@ -567,7 +625,8 @@ Q9: {
     npc: '',
     story: 'Good. Even living a life full of regrets is something you must accept in the end. You\'ve come far old friend.',
     question: 'Learn your fate?',
-      choices: [
+    image: '',  
+    choices: [
           {
               response: 'Yes.',
               path: 'Q10B'
@@ -580,7 +639,8 @@ Q9: {
     npc: 'Game Over',
     story: 'Cleary you\'ve learned nothing. Yes we\'ll try again',
     question: '',
-      choices: [
+    image: '',  
+    choices: [
           {
               response: 'Try again...',
               path: 'Q1'
@@ -593,7 +653,8 @@ Q10A: {
     npc: '',
     story: 'He leaves you in the forest to contemplate your choices, with the promise that he is only a call away. When you have fully accepted the weight of your decision, you are free to be released.',
     question: 'The End. Return home?',
-      choices: [
+    image: '',  
+    choices: [
           {
               response: 'Return Home.',
               path: 'Q1'
@@ -604,7 +665,8 @@ Q10A: {
     npc: '',
     story: 'You have acknowledged and accepted the wrongs committed in your life. You are aware there is no going back and a life with regrets is nothing to fear. He releases you from this place.',
     question: '',
-      choices: [
+    image: '',  
+    choices: [
           {
               response: 'The End. return home?',
               path: 'Q1'
@@ -615,4 +677,5 @@ Q10A: {
   
     // ----------------------------------------------------Function Calls-------------------------------------------------------------------
 
-initialLoad();
+getStorage();
+
